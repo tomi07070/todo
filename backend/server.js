1ae532065a389121a7dea74e49ac726909d08919
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-02-26 23:27:54 -0300

    back finished

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import dotenv from 'dotenv'
 import express from 'express'
 import ConnectDB from './db/Database.js'
 import router from './routes/todo.route.js'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 5000
 app.use(express.json())
+app.use(cors())
 
 dotenv.config()
 
