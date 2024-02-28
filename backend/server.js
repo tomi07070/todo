e0d16a76cd663fdd76c414e43f879baffae82d88
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-02-28 18:18:40 -0300

    finished

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,20 @@ import path from "path"
 
 const app = express()
 const port = process.env.PORT || 5000
+const __dirname = path.resolve()
 app.use(express.json())
 app.use(cors())
 
 dotenv.config()
 
 app.use("/", router)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 app.listen(port, ()=>{
     console.log(`server running on ${port}`)
